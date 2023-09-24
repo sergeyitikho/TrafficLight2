a51e5ba6b90685aa0df5bfa63cf78f34536ff591
swiftbook <swiftbook@macbook-pro-john.local>
2023-09-24 23:22:50 +0300

    Initial Commit

---
+++ TrafficLight2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TrafficLight2
+//
+//  Created by swiftbook on 24.09.2023.
+//  Copyright © 2023 swiftbook. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
